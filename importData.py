d3f2ee0707b1cf9cd4a8cbade24f4cfa17cd72fe
npchan1024 <npchan1024@gmail.com>
2018-09-30 20:50:01 -0700

    Committing my work from Sunday - general ideas of what I want to do with the data and the structure of a function, but as a script first.

--- importData.py
+++ importData.py
@@ -2,12 +2,12 @@
 import pandas as pd
 # Python doesn't recognize \ as a file location splitter, so I have to use /
 
-filename = 'C:/Users/NPC/Desktop/Career/Bytefoods/sampledata.csv'
-
+#filename = 'C:/Users/NPC/Desktop/Career/Bytefoods/sampledata.csv'
+filename = 'C:/Users/NPC/Desktop/Career/Bytefoods/items_purchased.csv'
 
 # Learn about what datatype the output for read_csv from pandas is, how to manipulate it, etc
 df = pd.read_csv(filename, sep=",")
-print(df)
+#print(df)
 
 print(df.loc[3][2])
 
@@ -15,6 +15,10 @@ print(df.loc[3][2])
 # Row 3 is the 4th entry (remember indexes start at 0)
 
 # This is a PANDAS DataFrame
+
+
+
+#remove data with fc_number = null
 
 
 
@@ -28,10 +32,19 @@ dp4167 = df.loc[(df.product_id == 4167), ["kiosk_id", "product_id", "card_hash",
 print(dp4167)
 
 
-# find subset of data of one user
+# analyze just the most frequent user
+# find the user with the most entries first, then use that subset
+
+user_counts = df["card_hash"].value_counts()
+print(user_counts)
+
 # pick this user pFCsO21elMCmPzQ4S0ASZesbTnPjb75q6sB4KEwR/SHlZJ4XlKB3wssrWb1cTRVYjEraXRaQssMiFUKXjJ0r3Q==
 du1= df.loc[(df.card_hash == "pFCsO21elMCmPzQ4S0ASZesbTnPjb75q6sB4KEwR/SHlZJ4XlKB3wssrWb1cTRVYjEraXRaQssMiFUKXjJ0r3Q=="), ["kiosk_id", "product_id", "card_hash", "date_time", "fc_number"]]
 print(du1)
+
+
+# Most common user: pxWdKu6voFceSKpehBo0XZ6EJF5N0UDXWvnd/EbM46cidlZpC1Md3ilg1NvM19ip8WAvilcMdy8siy5whPtAKA==
+du2= df.loc[(df.card_hash == "pxWdKu6voFceSKpehBo0XZ6EJF5N0UDXWvnd/EbM46cidlZpC1Md3ilg1NvM19ip8WAvilcMdy8siy5whPtAKA=="), ["kiosk_id", "product_id", "card_hash", "date_time", "fc_number"]]
 
 # Make a function/script that sorts the data by user, and does some analysis
 
@@ -43,9 +56,18 @@ print(du1)
 
 # create subset of data of one kiosk
 # Pick this kiosk - 548
+# 625 rows of data for this kiosk - lets look into that in a bit more detail first?
+
+# For this one kiosk, lets first just look at the most common product, and analyze the time that product is bought
+
+# Maybe we can look at products with the same category, and see if one fades out as another fades in?
+# If this is the case, we can apply that analysis to other kiosks, too
 dk548= df.loc[(df.kiosk_id == 548), ["kiosk_id", "product_id", "card_hash", "date_time", "fc_number"]]
 print(dk548)
 
+dk548sortdate = dk548.sort_values(by=["date_time"])
+print(dk548sortdate)
 
-
+dk548sortprod = dk548.sort_values(by=["product_id", "date_time"])
+print(dk548sortprod)
 
