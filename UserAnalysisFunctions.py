d3c321b1b4be50d96c621074a45a3ab761811bdb
npchan1024 <npchan1024@gmail.com>
2018-10-04 14:08:00 -0700

    Wrote everything as functions for now.  Made a shortcut function for the average time between purchases of the same product.

---
+++ UserAnalysisFunctions.py
@@ -0,0 +1,51 @@
+import pandas as pd
+
+def preProcess(filename):
+
+    # check that filename is not null
+
+    dataframe = pd.read_csv(filename, sep=",")
+
+    return dataframe
+
+# dataframe must have five fields of "kiosk_id", "product_id", "card_hash", "date_time", "fc_number"
+def getUserDataframe(dataframe, userhash):
+    # For now, these are the only fields.  Can make fields an input parameter to make this useful with other datasets.
+    fields = ["kiosk_id", "product_id", "card_hash", "date_time", "fc_number"]
+    datauser = dataframe.loc[(dataframe.card_hash == userhash), fields]
+    return datauser
+
+# This is meant to pass a single user's dataframe in and get analysis for a single product
+# Only returning kiosk id and date_time because we don't need the rest, can change this later
+def getSmallDataProduct(dataframe, prodid):
+    # for now data format is assumed to be valid
+    dataproduct = dataframe.loc[(dataframe.product_id == prodid), ["kiosk_id", "date_time"]]
+    return dataproduct
+
+
+def singleProductAnalysis(dataframe, userhash, prodid):
+    # A higher productAnalysis function should call this for each product id in the list.
+    freq = 1.234
+    return freq
+
+# need to pass in a dataframe with just kiosk_id and date_time... is there a way to redefine these dataframes?
+def shortAvgPurchaseWindow(dataframe):
+    # More universal to use this if I can make any field an input instead of just "date_time"
+    # Can save time if we already know the dataframe is sorted, but oh well
+
+    # Find latest date_time
+    maxdate = pd.to_datetime(max(dataframe["date_time"]))
+    # Find earliest date_time
+    mindate = pd.to_datetime(min(dataframe["date_time"]))
+    # Find number of values
+    count = dataframe["date_time"].count()
+    # Difference/number of values = avg time between purchases
+    #timedelta = maxdate-mindate
+    avgPurchaseWindow = (maxdate-mindate)//count
+    # Convert to something more displayable?
+    return avgPurchaseWindow
+
+def realAveragePurchaseWindow(dataframe, userhash, prodid):
+    # This should filter outliers and stuff, figure it out later.
+    purchaseWindow = 0
+    return purchaseWindow
--- UserProductAnalysis.py
+++ UserProductAnalysis.py
@@ -1,58 +1,22 @@
-import pandas as pd
+from UserAnalysisFunctions import *
 
+# The two inputs for my function should be a userhash?
 filename = 'C:/Users/NPC/Desktop/Career/Bytefoods/items_purchased.csv'
 userhash = "64LcsIctWnPrGaXfcW+gPRBJq5akh84HNEuUWbemoFZT8YOwmWpMjNDfRzyllfGGKXo37iHtiLOrIEW6XAePrw=="
-
-df = pd.read_csv(filename, sep=",")
+prodid = 2605
+# give a more easy to read name instead of df
+dataframe = preProcess(filename)
 
 # later on make this a function that you can input the df.card_hash for
-du = df.loc[(df.card_hash == userhash), ["kiosk_id", "product_id", "card_hash", "date_time", "fc_number"]]
 
-#dus = du.sort_values(by=['product_id', 'date_time'])
-#print(dus)
+# Supply variable for the access outside - check that dataframe is the right format first
 
-# check out product_id 2605 for this guy first
-# write something that does this for any product input later
-
-# function should be something like "analyze user/product"
-# should loop through every product in that user's list, and then find that frequency data and output for each product
-# see if there is a better way to do this in aggregate, or if I do need to just break the dataframe into smaller ones
-
-# first, do it for one product for this user - 2605
-prodid = 2605
-dup = du.loc[(du.product_id == prodid), ["kiosk_id", "date_time"]]
-dupsort = dup.sort_values(by=['date_time'])
-
-# reset indexes, makes it so we can loop through easier
-dupsort = dupsort.reset_index()
-print(dupsort)
-
-# this is just 45 rows of data - lets find the average time in between each purchase?
-# the first and second purchase have a long time in between, not sure how to account for it
-# make this into a function... next.
+datauser = getUserDataframe(dataframe, userhash)
+datauserproduct = getSmallDataProduct(datauser, prodid)
+datasorted = datauserproduct.sort_values(by=['date_time'])
+datasorted = datasorted.reset_index()
+datediff = shortAvgPurchaseWindow(datasorted)
+print("Average Purchase Window Time:")
+print(datediff)
 
 
-
-# date difference in PANDAS?
-# output it as one column, will be size n-1
-
-
-# Interestingly, there is a shortcut in the math....
-# Take difference of first and last purchase, and then divide by the number of purchases
-# What losses are there if we do this approximation - outlier data isn't accounted for
-
-
-# only important thing is to throw out outliers (customer went on vacation or something)
-
-# Think about this a little more, how do we tell if the user is "dropping off" at the end?
-
-# Series data structure
-# What tools in PANDAS is there for time series?
-
-# First datetime
-
-
-d1 = dupsort.loc[0][2]
-d2 = dupsort.loc[1][2]
-# The dates are somehow being treated as strings - how do we treat it as a date?
-print(d2-d1)
